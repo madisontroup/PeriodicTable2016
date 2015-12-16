43cbc70ff0efa6007f2ee4b3b1d89752d9f0e54b
madisontroup <madison.troup@gmail.com>
2015-12-16 09:29:58 -0500

    PUSH

--- app/src/main/java/com/kiskiarea/madisontroup/periodictable2016/MainActivity.java
+++ app/src/main/java/com/kiskiarea/madisontroup/periodictable2016/MainActivity.java
@@ -13,10 +13,10 @@ public class MainActivity extends
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // super.onCreate(savedInstanceState);
+     //   setContentView(R.layout.activity_main);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
